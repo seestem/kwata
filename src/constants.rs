99f11fe12f178ae28737148f775f2e8d4fe189db
Jackson G. Kaindume <cy6erlion@protonmail.com>
2024-06-16 02:19:30 +0200

    add balancesheets db name

--- src/constants.rs
+++ src/constants.rs
@@ -1,1 +1,2 @@
 pub const INCOME_STATEMENTS_DB: &str = "income_statements";
+pub const BALANCE_SHEETS_DB: &str = "balance_sheets";
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
-pub use constants::INCOME_STATEMENTS_DB;
+pub use constants::{BALANCE_SHEETS_DB, INCOME_STATEMENTS_DB};
+pub mod balance_sheets;
 pub mod constants;
 pub mod income_statement;
